ee406912c8a2c74968e5fa5f67ceacd99b48811c
Gene Heptonstall <gk.heptonstall@gmail.com>
2016-03-23 11:19:30 +0800

    fixed SparkFormHandling

--- mongodblearning/MongoDBLearning/src/main/java/org/gkh/sample/mongodblearning/SparkFormHandling.java
+++ mongodblearning/MongoDBLearning/src/main/java/org/gkh/sample/mongodblearning/SparkFormHandling.java
@@ -9,6 +9,7 @@ import freemarker.core.ParseException;
 import freemarker.template.Configuration;
 import freemarker.template.MalformedTemplateNameException;
 import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Arrays;
@@ -28,33 +29,59 @@ public class SparkFormHandling {
 
     public static void main(String[] args) {
         final Configuration config = new Configuration();
-        config.setClassForTemplateLoading(SparkFormHandling.class, "/");
+        config.setClassForTemplateLoading(SparkFormHandling.class, "/fruitpicker");
 
-        Spark.get("/", (Request rqst, Response rspns) -> {
+        Spark.get("/fruit", (Request rqst, Response rspns) -> {
 
-            StringWriter writer = new StringWriter();
-            try {
-                Template template = config.getTemplate("fruitpicker.ftl");
-
-                Map<String, Object> templateParamMap = new HashMap<>();
-                templateParamMap.put("fruits", Arrays.asList("Apples", "Oranges", "Bananas", "Mangoes"));
-
-                template.process(templateParamMap, writer);
-//                Map<String, Object> helloMap = new HashMap<>();
-//                helloMap.put("name", "SUPERGENE");
-//
-//                template.process(helloMap, writer);
-
-            } catch (MalformedTemplateNameException ex) {
-                Logger.getLogger(SparkFormHandling.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (ParseException ex) {
-                Logger.getLogger(SparkFormHandling.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (IOException ex) {
-                Logger.getLogger(SparkFormHandling.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            return writer;
+            return processFruitWriter(config);
         });
 
+        Spark.get("/fruit/:name", (Request rqst, Response rspns) -> {
+            String name = rqst.params("name");
+
+            return processFruitWriter(config, name);
+        });
+
+        Spark.post("/favourite_fruit", (Request rqst, Response rspns) -> {
+            String pickedFruit = rqst.queryParams("fruit");
+            String name = rqst.params("name");
+            if (pickedFruit == null || pickedFruit.length() == 0) {
+                return "Why didn't you pick a fruit?";
+            } else {
+                return "Favourite fruit: " + pickedFruit;
+            }
+//            return new Object();
+        });
+
+    }
+
+    private static StringWriter processFruitWriter(Configuration config) {
+        return processFruitWriter(config, null);
+    }
+
+    private static StringWriter processFruitWriter(Configuration config, String name) {
+        StringWriter writer = new StringWriter();
+        try {
+            Template template = config.getTemplate("fruitpicker.ftl");
+
+            Map<String, Object> templateParamMap = new HashMap<>();
+            templateParamMap.put("fruits", Arrays.asList("Apples", "Oranges", "Bananas", "Mangoes"));
+            if (name == null || name.length() == 0) {
+                templateParamMap.put("name", "DEFAULT");
+            } else {
+                templateParamMap.put("name", name);
+            }
+
+            template.process(templateParamMap, writer);
+
+        } catch (MalformedTemplateNameException ex) {
+            Logger.getLogger(SparkFormHandling.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(SparkFormHandling.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException | TemplateException ex) {
+            Logger.getLogger(SparkFormHandling.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return writer;
     }
 
 }
