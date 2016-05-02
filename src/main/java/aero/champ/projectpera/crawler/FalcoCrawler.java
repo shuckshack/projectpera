89c838d62c6cbd10eb34fe5cacf9464bf293d047
Roy Marco Aruta <marco.aruta@champ.aero>
2016-05-02 11:32:57 +0800

    Added export to CSV on FalcoWeb

--- src/main/java/aero/champ/projectpera/crawler/FalcoCrawler.java
+++ src/main/java/aero/champ/projectpera/crawler/FalcoCrawler.java
@@ -1,10 +1,12 @@
 package aero.champ.projectpera.crawler;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,18 +16,24 @@ import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.ScriptResult;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.attachment.Attachment;
 import com.gargoylesoftware.htmlunit.html.HtmlDivision;
+import com.gargoylesoftware.htmlunit.html.HtmlInlineFrame;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTableDataCell;
 
 public class FalcoCrawler extends Thread {
 
+	private static final String FALCO_DOMAIN = "http://falcoweb.champ.aero";
+
 	private static final Logger LOG = LoggerFactory.getLogger(FalcoCrawler.class);
 
-	private static final String END_DATE = "15/04/2016";
+	private static final String START_DATE = "16/04/2016";
 
-	private static final String START_DATE = "01/04/2016";
+	private static final String END_DATE = "30/04/2016";
+
+	private static final String FILE_DATE = "02MAY16-";
 
 	private static final String EMPLOYEE_CARD_NO = "25370";
 
@@ -33,7 +41,7 @@ public class FalcoCrawler extends Thread {
 
 	private static final String URL = "http://falcoweb.champ.aero/FALCOWEB/(S(3wr5vyiqzhg3icucabjj31aq))/Report/TAReport.aspx";
 	
-	private static final String OUT_FOLDER = "out/";
+	private static final String OUT_FOLDER = ".//out//";
 	
 	private WebClient client;
 
@@ -124,7 +132,7 @@ public class FalcoCrawler extends Thread {
 			}
 			
 			getReportHtml(pageResult);
-			getReportPdf(pageResult);
+//			getReportPdf(pageResult);
 			getReportCsv(pageResult);
 		} catch (Exception e) {
 			LOG.error(e.getMessage(), e);
@@ -162,8 +170,9 @@ public class FalcoCrawler extends Thread {
 		LOG.info(saveFormat.getId());
 		saveFormat.setValueAttribute("Pdf");
 
+		pageResult.executeJavaScript("var event = document.createEvent('Event');");
 		ScriptResult result = pageResult
-				.executeJavaScript("aspxMIClick('', 'ReportToolbar1_Menu', '11')");
+				.executeJavaScript(pdfExport.getOnClickAttribute());
 
 		try {
             Page saveResult  = result.getNewPage();
@@ -174,7 +183,8 @@ public class FalcoCrawler extends Thread {
 			if (saveResult.isHtmlPage()) {
 				exportToFile("Result_"+System.currentTimeMillis()+".html", saveResult.getWebResponse().getContentAsStream());
 			} else {
-				exportToFile("Result_"+System.currentTimeMillis()+".pdf", saveResult.getWebResponse().getContentAsStream());
+                Attachment attch = new Attachment(saveResult);
+                exportToFile(attch.getSuggestedFilename(), saveResult.getWebResponse().getContentAsStream());
 			}
 		} catch (IOException e) {
 			LOG.error(e.getMessage(), e);
@@ -182,43 +192,54 @@ public class FalcoCrawler extends Thread {
 	}
 
 	public void getReportCsv(HtmlPage pageResult) {
-		HtmlTableDataCell pdfExport = (HtmlTableDataCell) pageResult
-				.getFirstByXPath("//td[contains(@title, 'Export a report and save it to the disk')]");
-		LOG.info(pdfExport.getAttribute("title"));
+		try {
+            pageResult.getWebClient().getOptions().setThrowExceptionOnScriptError(false);
+			pageResult.initialize();
+		} catch (Exception e) {
+			LOG.error(e.getMessage(), e);
+		}
 		
 		try {
             HtmlInput saveFormat = (HtmlInput) pageResult.getFirstByXPath("//input[contains(@id, 'SaveFormat_VI')]");
             LOG.info(saveFormat.getId());
-			saveFormat.click();
             saveFormat.setValueAttribute("csv");
 			LOG.info(saveFormat.getValueAttribute());
+			pageResult.executeJavaScript(saveFormat.getOnChangeAttribute());
 
             HtmlInput saveFormat2 = (HtmlInput) pageResult.getFirstByXPath("//input[contains(@id, 'SaveFormat_I')]");
             LOG.info(saveFormat2.getId());
-			saveFormat2.click();
             saveFormat2.setValueAttribute("Csv");
 			LOG.info(saveFormat2.getValueAttribute());
-
-		} catch (IOException e) {
+			pageResult.executeJavaScript(saveFormat.getOnChangeAttribute());
+		} catch (Exception e) {
 			LOG.error(e.getMessage(), e);
 		}
-		
-//        ScriptResult result = pageResult.executeJavaScript("aspxETextChanged('ReportToolbar1_Menu_ITCNT13_SaveFormat')");
-//        LOG.info(result.getJavaScriptResult().toString());
-		
-		ScriptResult result = pageResult
-				.executeJavaScript("aspxMIClick('', 'ReportToolbar1_Menu', '11')");
-		LOG.info(result.getJavaScriptResult().toString());
 
+		HtmlTableDataCell pdfExport = (HtmlTableDataCell) pageResult
+				.getFirstByXPath("//td[contains(@title, 'Export a report and save it to the disk')]");
+		LOG.info(pdfExport.getAttribute("title"));
+		
+		pageResult.executeJavaScript("var event = document.createEvent('Event');");
+		ScriptResult result = pageResult
+				.executeJavaScript(pdfExport.getOnClickAttribute());
+		LOG.info("Script: " + result.getJavaScriptResult().toString());
+		
 		try {
-            Page saveResult  = result.getNewPage();
+            Page saveResult = pageResult;
 
             LOG.info("POST: " + saveResult.getWebResponse().getWebRequest().toString());
             LOG.info("IsHtml: " + saveResult.isHtmlPage());
             LOG.info("URL: " + saveResult.getUrl().toString());
 			
 			if (saveResult.isHtmlPage()) {
-				exportToFile("Result_"+System.currentTimeMillis()+".html", saveResult.getWebResponse().getContentAsStream());
+                HtmlInlineFrame frame = (HtmlInlineFrame) pageResult.getFirstByXPath("//iframe[contains(@id, 'DXPrinter')]");
+                String src = frame.getSrcAttribute();
+                LOG.info("DXPrinter Src: " + src);
+
+                Page page = client.getPage(FALCO_DOMAIN + src);
+                LOG.info("Page is HTML? " + page.isHtmlPage());
+                Attachment attch = new Attachment(page);
+                exportToFile(attch.getSuggestedFilename(), page.getWebResponse().getContentAsStream());
 			} else {
 				exportToFile("Result_"+System.currentTimeMillis()+".csv", saveResult.getWebResponse().getContentAsStream());
 			}
@@ -227,22 +248,25 @@ public class FalcoCrawler extends Thread {
 		}
 	}
 	
-	public void exportToFile(String outputFile, InputStream is) {
+	public void exportToFile(String outputFile, InputStream in) {
 		OutputStream os = null;
-		try {
-			os = new FileOutputStream(OUT_FOLDER + outputFile);
-			int read = 0;
-			byte[] bytes = new byte[1024];
 
-			while ((read = is.read(bytes)) != -1) {
-				os.write(bytes, 0, read);
-			}
-		} catch (Exception e) {
+		try {
+			File outFolder = new File(OUT_FOLDER);
+            if (!outFolder.exists()) {
+                outFolder.mkdir();
+            }
+
+            File outFile = new File(OUT_FOLDER + FILE_DATE + outputFile);
+			os = new FileOutputStream(outFile);
+
+            IOUtils.copy(in, os);
+		} catch (IOException e) {
 			LOG.error(e.getMessage(), e);
 		} finally {
 			try {
-				if (is != null) {
-					is.close();
+				if (in != null) {
+					in.close();
 				}
 				if (os != null) {
 					os.close();
