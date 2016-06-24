870de5db44ef4ba0650effefd9f5889e56346f19
unknown <batge@LT7CCSP021.champ.aero>
2016-06-24 19:40:03 +0800

    test commit

--- src/main/java/aero/champ/projectpera/scheduler/scheduled/ReportGenerator.java
+++ src/main/java/aero/champ/projectpera/scheduler/scheduled/ReportGenerator.java
@@ -1,6 +1,12 @@
 package aero.champ.projectpera.scheduler.scheduled;
 
+/**
+ * The Interface ReportGenerator.
+ */
 public interface ReportGenerator {
 
+	/**
+	 * Generate cut off report.
+	 */
 	public void generateCutOffReport();
 }
