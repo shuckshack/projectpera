606521afc58135bad7364904f41eee09d122e7e5
Dominic Diamante <dominicpimentel.diamante@champ.aero>
2016-08-02 13:05:59 +0800

    Class for storing and retrieving Employee Detail objects

---
+++ src/main/java/aero/champ/projectpera/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package aero.champ.projectpera.repository;
+
+public class EmployeeRepository extends MongoDbRepository {
+
+	public EmployeeRepository(MongoDbConnector connector) {
+		super(connector);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
