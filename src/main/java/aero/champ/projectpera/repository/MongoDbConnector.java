3535a124002156103ee1a611f2fd4d50d4c3b4c0
Dominic Diamante <dominicpimentel.diamante@champ.aero>
2016-08-02 12:49:29 +0800

    Repository artifact refactoring

--- src/main/java/aero/champ/projectpera/repository/MongoDbConnector.java
+++ src/main/java/aero/champ/projectpera/repository/MongoDbConnector.java
@@ -3,32 +3,45 @@ package aero.champ.projectpera.repository;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 
-import aero.champ.projectpera.util.PropertyFile;
+/*import aero.champ.projectpera.util.PropertyFile;*/
 
 public class MongoDbConnector implements DatabaseConnector {
 	
-	public static final String CONFIG_FILE = "mongodb.properties";
+	/*public static final String CONFIG_FILE = "mongodb.properties";
 	public static final String KEY_HOST = "db.mongo.host";
 	public static final String KEY_PORT = "db.mongo.port";
-	public static final String KEY_DB = "db.mongo.database";
+	public static final String KEY_DB = "db.mongo.database";*/
 	
 	private MongoDatabase database;
 	private MongoClient mongoClient;
+	private String databaseName;
 
+	public MongoDbConnector(MongoClient mongoClient, String databaseName) {
+		this.mongoClient = mongoClient;
+		this.databaseName = databaseName;
+	}
+	
 	@Override
 	public void openConnection() throws Exception {
-		PropertyFile propertyFile = new PropertyFile(CONFIG_FILE);
-		String host = propertyFile.getValue(KEY_HOST);
-		Integer port = Integer.parseInt(propertyFile.getValue(KEY_PORT));
-		String databaseName = propertyFile.getValue(KEY_DB);
 		
-		mongoClient = new MongoClient(host, port);
+		/*if (mongoClient == null) {
+			PropertyFile propertyFile = new PropertyFile(CONFIG_FILE);
+			String host = propertyFile.getValue(KEY_HOST);
+			Integer port = Integer.parseInt(propertyFile.getValue(KEY_PORT));
+			String databaseName = propertyFile.getValue(KEY_DB);
+		}
+		
+		mongoClient = new MongoClient(host, port);*/
 		database = mongoClient.getDatabase(databaseName);
 	}
 
 	@Override
 	public void closeConnection() {
 		mongoClient.close();
-	}	
+	}
+	
+	protected MongoDatabase getDatabase() {
+		return this.database;
+	}
 	
 }
