4b300f2a1e9570e20f293c85cf98a824c40aa5c6
Ralph Sacdalan <ralph.sacdalan@champ.aero>
2016-08-12 20:56:42 +0800

    Workaround on generating the List<EmployeeDetails>

--- src/main/java/aero/champ/projectpera/parser/EmployeeTimeRecorder.java
+++ src/main/java/aero/champ/projectpera/parser/EmployeeTimeRecorder.java
@@ -1,11 +1,19 @@
 package aero.champ.projectpera.parser;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import aero.champ.projectpera.BO.EmployeeDetails;
+import aero.champ.projectpera.BO.TimeInOut;
 import aero.champ.projectpera.sql.bean.FalcoEmployee;
 import aero.champ.projectpera.sql.dao.FalcoTransactionsDao;
 
@@ -17,7 +25,7 @@ public class EmployeeTimeRecorder {
 		this.dao = new FalcoTransactionsDao();
 	}
 	
-	public List<List<FalcoEmployee>> getFirstInLastOut(String dailyDate) {
+	public List<List<FalcoEmployee>> getAllEmployeeInOutList(String dailyDate) {
 		
 		List<FalcoEmployee> falcoCompleteEmployeeeList = this.dao.getDetails(dailyDate);
 		
@@ -49,56 +57,199 @@ public class EmployeeTimeRecorder {
 		
 	}
 	
-	public static void main(String[] args) throws IOException {
+	public List<EmployeeDetails> get() {
 		
-		EmployeeTimeRecorder recorder = new EmployeeTimeRecorder();
+		List<EmployeeDetails> empList = new ArrayList<EmployeeDetails>();
 		
-		int startDay = 16;
-		int endDay = 31;
+		// Filename: testfileYYYY_MM_DD.txt
+		// CSV format: NAME,CARD_NO,FIRST_IN,LAST_OUT
+		// Users: CARD_NO,NAME 
+		
+		String filenameTemplate = "testfile";
+		String dateTemplate = "2016_08_";
+		String directory = "D:/timesheet/20160816/";
+		
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+		
+		try {
+			
+			List<String> userList = new ArrayList<String>();
+			
+			BufferedReader userReader = new BufferedReader(new FileReader("D:/timesheet/ALL_USERS.txt"));
+			String userLine;
+		    while ((userLine = userReader.readLine()) != null) {
+//			    System.out.println(userLine);
+		    	userList.add(userLine);
+			}
+		    userReader.close();
+		    userReader = null;
+		    
+			for (String user: userList) {
+				
+				String[] userDetails = user.split("\\|");
+				
+				String cardNo = userDetails[0].trim();
+				String name = userDetails[1].trim();
+				
+				EmployeeDetails employee = new EmployeeDetails();
+				
+				employee.setLastName(name);
+				employee.setCardNumber(Integer.parseInt(cardNo));
+				
+				List<TimeInOut> timeInOutList = new ArrayList<TimeInOut>();
+				
+				int startDay = 1;
+				int endDay = 15;
+				
+				for (int day = startDay; day <= endDay; day++) {
+					
+					String fileNameDate = dateTemplate + String.format("%02d", day);
+					
+					String filename = filenameTemplate + fileNameDate;
+					
+					BufferedReader reader = new BufferedReader(new FileReader(directory + filename + ".txt"));
+					
+					String line;
+				    while ((line = reader.readLine()) != null) {
 
-		PrintStream writer = new PrintStream(new File("d:/testfile.txt"));
+				    	if (line.contains(cardNo)) {
+				    		String[] empTimeDetails = line.split("\\|");
+				    		
+				    		Date timeIn = new Date();
+				    		Date timeOut = new Date();
+				    		
+				    		try {
+					    		timeIn = simpleDateFormat.parse(empTimeDetails[2]);
+					    		timeOut = simpleDateFormat.parse(empTimeDetails[3]);
+				    		} catch (ArrayIndexOutOfBoundsException e) {
+				    			System.out.println(line);
+				    			e.printStackTrace();
+				    		}
+				    		
+				    		TimeInOut timeInOut = new TimeInOut();
+				    		timeInOut.setTimeIn(timeIn);
+				    		timeInOut.setTimeOut(timeOut);
+				    		
+				    		timeInOutList.add(timeInOut);
+				    		
+				    		break;
+				    	}
+				    	
+				    }
+				    
+				    reader.close();
+				    reader = null;
+				}
+				
+				employee.setTimeInOutList(timeInOutList);
+				empList.add(employee);
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return empList;
+	}
+	
+	public void test() throws Exception {
+		
+		int startDay = 1;
+		int endDay = 15;
+
+		PrintStream writer;
 		
 		// YYYY/MM/
-		String dateTemplate = "2016/07/";
+		String dateTemplate = "2016/08/";
 		
 		for (int day = startDay; day <= endDay; day++) {
 			
-			String queryDate = dateTemplate + day;
+			String queryDate = dateTemplate + String.format("%02d", day);
+			
+			String filenameDate = queryDate.replaceAll("/", "_");
+			
+			writer = new PrintStream(new File("d:/timesheet/20160816/testfile"+filenameDate+".txt"));
 			
 			System.out.println(queryDate);
 			
-			writer.println("===================================================================== \n\n");
-			writer.println(queryDate);
-			
-			List<List<FalcoEmployee>> list = recorder.getFirstInLastOut(queryDate);
+			List<List<FalcoEmployee>> list = getAllEmployeeInOutList(queryDate);
 			
 			for (List<FalcoEmployee> oneEmployee: list) {
 				
-				System.out.println("Employee: ");
+				String empName = new String();
 				
 				if (oneEmployee != null && oneEmployee.size() > 0) {
-					System.out.println(oneEmployee.get(0).getTrName());
-					writer.println(oneEmployee.get(0).getTrName());
+					empName = oneEmployee.get(0).getTrName();
 				}
+				
+				// NAME,CARD_NO,FIRST_IN,LAST_OUT
+				String empDetailsCsv = empName;
 				
 				for (int i = 0; i < oneEmployee.size(); i++) {
 					
 					FalcoEmployee empDetails = oneEmployee.get(i);
 					
-					if (i == 0 || i == oneEmployee.size()-1) {
-						System.out.println("CardNo: " + empDetails.getCardNo() + " | Date: " + empDetails.getTrDate() + " | Time: " + empDetails.getTrTime());
-						writer.println("CardNo: " + empDetails.getCardNo() + " | Date: " + empDetails.getTrDate() + " | Time: " + empDetails.getTrTime());
+					if (i == 0) {
+						// first in
+						empDetailsCsv = empDetailsCsv + "|" + empDetails.getCardNo() + "|" + empDetails.getTrTime();
+					} else if (i == oneEmployee.size()-1) {
+						// last out
+						empDetailsCsv = empDetailsCsv + "|" + empDetails.getTrTime();
 					}
 					
 				}
 				
+				writer.println(empDetailsCsv);
+				
+			}
+			
+			writer.flush();
+			writer.close();
+			writer = null;
+		}
+		
+	}
+	
+	public void queryTimes() {
+		try {
+			test();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void testEmpDetailsList() {
+		List<EmployeeDetails> list = get();
+		
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+		
+		for (EmployeeDetails emp: list) {
+			if (emp.getTimeInOutList() != null && emp.getTimeInOutList().size() > 0) {
+				System.out.println(emp.getLastName() + " " + emp.getCardNumber() + " ");
+				for (TimeInOut timeInOut: emp.getTimeInOutList()) {
+					
+					System.out.print(simpleDateFormat.format(timeInOut.getTimeIn()) + " ");
+					System.out.println(simpleDateFormat.format(timeInOut.getTimeOut()));
+				}
 			}
 			
 		}
+	}
+	
+	public static void main(String[] args) throws IOException {
 		
-		writer.flush();
-		writer.close();
-		writer = null;
+		EmployeeTimeRecorder recorder = new EmployeeTimeRecorder();
+
+		// recorder.queryTimes();
+		recorder.testEmpDetailsList();
 		
 	}
 }
