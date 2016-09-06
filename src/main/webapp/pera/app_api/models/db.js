2c3133c7a26374408632af6b36828f601b06f45b
CHING Emmanuel <chiem@champ.aero>
2016-09-06 21:13:30 +0800

    MEAN Webapp Update
    1. Implement Crowd for authentication

--- src/main/webapp/pera/app_api/models/db.js
+++ src/main/webapp/pera/app_api/models/db.js
@@ -42,3 +42,4 @@ process.on('SIGTERM', function() {
 });
 
 require('./staff');
+require('./users');
--- src/main/webapp/pera/app_client/login/login.controller.js
+++ src/main/webapp/pera/app_client/login/login.controller.js
@@ -1,42 +1,32 @@
-(function () {
+(function() {
   angular
     .module('peraApp')
     .controller('loginCtrl', loginCtrl);
   
-  loginCtrl.$inject = ['$location','authentication'];
+  loginCtrl.$inject = ['$location', 'authentication'];
   function loginCtrl($location, authentication) {
     var vm = this;
     
-    vm.pageHeader = {
-      title: 'Sign in to Loc8r'
-    };
-
     vm.credentials = {
       email : "",
       password : ""
     };
-
-    vm.returnPage = $location.search().page || '/';
-
-    vm.onSubmit = function () {
+    
+    vm.onSubmit = function() {
       vm.formError = "";
-      if (!vm.credentials.email || !vm.credentials.password) {
-        vm.formError = "All fields required, please try again";
+      if (!vm.credentials.username || !vm.credentials.password) {
+        vm.formError = "All fields required, please try again.";
         return false;
       } else {
-        vm.doLogin();
+        authentication
+          .login(vm.credentials)
+          .error(function(err) {
+            vm.formError = err;
+          })
+          .success(function(user) {
+            $location.path('/profile/' + user.cardNumber);
+          });
       }
-    };
-
-    vm.doLogin = function() {
-      vm.formError = "";
-      authentication
-        .login(vm.credentials)
-        .error(function(err){
-          vm.formError = err;
-        })
-        .then(function(){
-        });
     };
   }
 })();
