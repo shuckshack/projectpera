16d9ea17cc249b404f417afc9667d9e4679a5c86
Gene Heptonstall <gk.heptonstall@gmail.com>
2016-09-07 16:34:56 +0800

    implement MongoDBEmployeeRepository.getEmployeeList, add JUnit test for it

--- src/main/java/aero/champ/projectpera/repository/MongoDbEmployeeRepository.java
+++ src/main/java/aero/champ/projectpera/repository/MongoDbEmployeeRepository.java
@@ -3,12 +3,17 @@ package aero.champ.projectpera.repository;
 import org.bson.Document;
 
 import com.google.gson.Gson;
-import com.mongodb.DBObject;
-import com.mongodb.util.JSON;
+
+import static com.mongodb.client.model.Projections.fields;
+import static com.mongodb.client.model.Projections.excludeId;
+import static com.mongodb.client.model.Sorts.orderBy;
+import static com.mongodb.client.model.Sorts.ascending;
 
 import java.util.List;
 
 import aero.champ.projectpera.BO.EmployeeDetails;
+import java.util.ArrayList;
+import org.bson.conversions.Bson;
 
 public class MongoDbEmployeeRepository extends MongoDbRepository implements EmployeeRepository {
 
@@ -28,10 +33,27 @@ public class MongoDbEmployeeRepository extends MongoDbRepository implements Empl
 		
 	}
 
+        /**
+         * Get list of all employees sort by cardNumber ascending. Exclude _id field.
+         * 
+         * @return List 
+         */
 	@Override
 	public List<EmployeeDetails> getEmployeeList() {
-		// TODO Auto-generated method stub
-		return null;
+                Bson projection = fields(excludeId());
+                Bson sort = orderBy(ascending("cardNumber"));
+                List<EmployeeDetails> empDetails = new ArrayList<>();
+                List<Document> empList = getCollection()
+                        .find()
+                        .projection(projection)
+                        .sort(sort)
+                        .into(new ArrayList<Document>());
+                Gson gson = new Gson();
+                for (Document empDoc : empList) {
+                    EmployeeDetails empDetail = gson.fromJson(empDoc.toJson(), EmployeeDetails.class);
+                    empDetails.add(empDetail);
+                }
+                return empDetails;
 	}
 
 	@Override
--- src/test/java/aero/champ/projectpera/repository/MongoDbEmployeeRepositoryTest.java
+++ src/test/java/aero/champ/projectpera/repository/MongoDbEmployeeRepositoryTest.java
@@ -3,20 +3,29 @@ package aero.champ.projectpera.repository;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import aero.champ.projectpera.BO.EmployeeDetails;
 
 import com.mongodb.MongoClient;
+import org.apache.log4j.Logger;
+
+import static org.junit.Assert.assertTrue;
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
 public class MongoDbEmployeeRepositoryTest {
 
-	private EmployeeRepository employeeRepository;
+	private static EmployeeRepository employeeRepository;
+        
+        private static final Logger LOG = Logger.getLogger(MongoDbEmployeeRepositoryTest.class);
 	
-	@Before
-	public void setUp() throws Exception {
+	@BeforeClass
+	public static void setUp() throws Exception {
+            LOG.info("=============BeforeClass============");
 		MongoClient mongoClient = new MongoClient("vl29.champ.aero", 27017);
 		
 		MongoDbConnector connector = new MongoDbConnector(mongoClient, "hrdb");
@@ -27,13 +36,23 @@ public class MongoDbEmployeeRepositoryTest {
 		((MongoDbEmployeeRepository) employeeRepository).initializeCollection();
 	}
 	
-	@After
-	public void tearDown() throws Exception {
+	@AfterClass
+	public static void tearDown() throws Exception {
+            LOG.info("=============AfterClass============");
+                ((MongoDbEmployeeRepository) employeeRepository).getCollection()
+                        .deleteMany(
+                                and(
+                                        eq("firstName", "Napoleon"),
+                                        eq("lastName","Bonaparte"),
+                                        eq("cardNumber",370118)
+                                )
+                        );
 		((MongoDbEmployeeRepository) employeeRepository).closeRepository();
 	}
 	
 	@Test
 	public void testInsertEmployeeList() {
+            LOG.info("=============testInsertEmployeeList============");
 		EmployeeDetails napoleonBonaparte = new EmployeeDetails();
 		napoleonBonaparte.setFirstName("Napoleon");
 		napoleonBonaparte.setLastName("Bonaparte");
@@ -43,6 +62,27 @@ public class MongoDbEmployeeRepositoryTest {
 		employeeDetailList.add(napoleonBonaparte);
 		
 		employeeRepository.insertEmployeeList(employeeDetailList);
+                List<Document> napoleonEmployee = ((MongoDbEmployeeRepository) employeeRepository).getCollection()
+                        .find(
+                                eq("cardNumber", 370118)
+                        )
+                        .into(new ArrayList<Document>());
+                assertTrue(napoleonEmployee.size() == 1);
 	}
+        
+    @Test
+    public void testGetEmployeeList() {
+        LOG.info("=============testGetEmployeeList============");
+        List<EmployeeDetails> empDetailsList = employeeRepository.getEmployeeList();
+        assertTrue(empDetailsList.size() > 0);
+        StringBuilder logInfo;
+        for (EmployeeDetails empDetails : empDetailsList) {
+            logInfo = new StringBuilder();
+            logInfo.append("card number: ").append(empDetails.getCardNumber());
+            logInfo.append("|first name: ").append(empDetails.getFirstName());
+            logInfo.append("|last name: ").append(empDetails.getLastName());
+            LOG.info(logInfo.toString());
+        }
+    }
 	
 }
