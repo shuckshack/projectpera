06f89d4d1675937093445eecdca89bd6f88c8668
GERALD BONILLA <gerald.bonilla@champ.aero>
2016-10-03 20:45:15 +0800

    Insert Update + Refactor

--- src/main/java/aero/champ/projectpera/repository/EmployeeRepository.java
+++ src/main/java/aero/champ/projectpera/repository/EmployeeRepository.java
@@ -1,15 +1,47 @@
 package aero.champ.projectpera.repository;
 
-import java.util.List;
-
 import aero.champ.projectpera.BO.EmployeeDetails;
 
-public interface EmployeeRepository {
+import java.util.List;
 
+
+/**
+ * DOCUMENT ME!
+ *
+ * @version  $Revision$, $Date$
+ */
+public interface EmployeeRepository
+{
+	//~ Methods ----------------------------------
+
+	/**
+	 * DOCUMENT ME!
+	 *
+	 * @param  employeeDetails
+	 */
 	void insertEmployeeList(List<EmployeeDetails> employeeDetails);
 	
+
+	/**
+	 * DOCUMENT ME!
+	 *
+	 * @return
+	 */
 	List<EmployeeDetails> getEmployeeList();
 	
+
+	/**
+	 * DOCUMENT ME!
+	 *
+	 * @param  employeeDetails
+	 */
 	void updateEmployee(EmployeeDetails employeeDetails);
 	
+
+	/**
+	 * DOCUMENT ME!
+	 *
+	 * @param  employeeDetails
+	 */
+	void insertUpdateEmployeeList(List<EmployeeDetails> employeeDetails);
 }
