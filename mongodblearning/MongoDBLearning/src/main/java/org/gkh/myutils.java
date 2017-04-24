d6661207888c7898bbdcb62f2673060464ef169d
Gene Heptonstall <gk.heptonstall@gmail.com>
2017-04-24 17:34:07 +0800

    update code to create FWB files in folders

--- mongodblearning/MongoDBLearning/src/main/java/org/gkh/myutils.java
+++ mongodblearning/MongoDBLearning/src/main/java/org/gkh/myutils.java
@@ -8,6 +8,8 @@ package org.gkh;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,7 +19,7 @@ import java.util.logging.Logger;
  */
 public class myutils {
 
-    private static final String MQ_MSG_FILE_DIR = "D:\\Documents\\2017-04-17 CSP-95712\\";
+    private static final String MQ_MSG_FILE_DIR = "D:\\Documents\\2017-04-17 CSP-95712\\AWB\\";
 
     private static final int COUNTER = 10;
 
@@ -25,55 +27,56 @@ public class myutils {
 
     private static final String AWB_NUMBER_PREFIX = "160-";
 
-    private static final String FWB_START = "FWB/14";
+    private static final String FWB_START = "FWB/14\n";
 
-    private static final String AWB_ORIG_DEST = "HKGMNL/";
-
-    private static final String AWB_PCS_WGT_VOL = "T500K45MC0.01";
-
-    private static final String FWB_BODY = "FLT/CX963P/21"
-            + "\nRTG/MNLCX"
-            + "\nSHP"
-            + "\n/SUPER GENE CASS"
-            + "\n/1 SUPER STREET"
-            + "\n/SUPER HK CITY"
-            + "\n/HK"
-            + "\nCNE"
-            + "\n/SUPER GENE CASS LHR"
-            + "\n/1 SUPER STREET"
-            + "\n/SUPER CITY"
-            + "\n/GB"
-            + "\nAGT//0051010/5400"
-            + "\n/GLT-CASS"
-            + "\n/HONGKONG"
-            + "\nCVD/HKD/PP/PP/NVD/NCV/XXX"
-            + "\nRTD/1/P500/K45/CQ/W45/R23.00/T1035.00"
-            + "\n/NG/UNK"
-            + "\n/2/NV/MC0.01"
-            + "\nOTH/P/SCC200.00"
-            + "\n/P/MYC450.00"
-            + "\nPPD/WT1035.00"
-            + "\n/OC650.00/CT1685.00"
-            + "\nISU/21APR17/HKG"
-            + "\nREF/EZYFRHT";
+    private static final String FWB_BODY = "HKGLHR/T500K45MC0.01\n"
+            + "FLT/CX4202/24\n"
+            + "RTG/LHRCX\n"
+            + "SHP\n"
+            + "/SUPER GENE CASS\n"
+            + "/1 SUPER STREET\n"
+            + "/SUPER HK CITY\n"
+            + "/HK\n"
+            + "CNE\n"
+            + "/SUPER GENE CASS LHR\n"
+            + "/1 SUPER STREET\n"
+            + "/SUPER CITY\n"
+            + "/GB\n"
+            + "AGT//4200999/0002\n"
+            + "/SUPER GENE CASS\n"
+            + "/SUPER HK CITY\n"
+            + "CVD/HKD/PP/PP/NVD/NCV/XXX\n"
+            + "RTD/1/P500/K45/CQ/W45/R66.20/T2979.00\n"
+            + "/NG/CONSOL\n"
+            + "/2/NV/MC0.01\n"
+            + "OTH/P/SCC45.00\n"
+            + "/P/MYC45.00\n"
+            + "PPD/WT2979.00\n"
+            + "/OC90.00/CT3069.00\n"
+            + "ISU/24APR17/HKG\n"
+            + "REF/EZYFRHT";
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int awbNumber = 727533;
-        int maxAwbNumber = 727733;
-        File mqMsgFileDir = new File(MQ_MSG_FILE_DIR);
+        int awbNumber = 730533;
+        int maxAwbNumber = 730534;
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat format = new SimpleDateFormat("ddMMMyy-HHmmss");
+        String mqMsgFileDirPath = MQ_MSG_FILE_DIR + format.format(cal.getTime()) + File.separatorChar;
+        File mqMsgFileDir = new File(mqMsgFileDirPath);
+
         if (!mqMsgFileDir.exists()) {
             mqMsgFileDir.mkdirs();
         }
+        
         File mqMsgFile;
         StringBuilder fileNameBuilder;
         String awbSerial;
         do {
             awbSerial = createAWBSerial(awbNumber);
-            
-            fileNameBuilder = new StringBuilder(MQ_MSG_FILE_DIR);
+            fileNameBuilder = new StringBuilder(mqMsgFileDirPath);
             fileNameBuilder.append(awbSerial);
             fileNameBuilder.append(MQ_MSG_FILE_SUFFIX);
 
@@ -88,22 +91,18 @@ public class myutils {
         } while (awbNumber < maxAwbNumber);
 
     }
-    
+
     private static String createAWBSerial(int awbNumber) {
         StringBuilder awbNumberBuilder = new StringBuilder(AWB_NUMBER_PREFIX);
         awbNumberBuilder.append(String.format("%7s", String.valueOf(awbNumber)).replace(' ', '0'));
         awbNumberBuilder.append(awbNumber % 7);
-        
+
         return awbNumberBuilder.toString();
     }
 
     private static String createFWB(String awbSerial) {
         StringBuilder fwbMessageBuilder = new StringBuilder(FWB_START);
-        fwbMessageBuilder.append("\n");
         fwbMessageBuilder.append(awbSerial);
-        fwbMessageBuilder.append(AWB_ORIG_DEST);
-        fwbMessageBuilder.append(AWB_PCS_WGT_VOL);
-        fwbMessageBuilder.append("\n");
         fwbMessageBuilder.append(FWB_BODY);
 
         return fwbMessageBuilder.toString();
